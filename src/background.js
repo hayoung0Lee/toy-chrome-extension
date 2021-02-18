c7d784c0d3405409e377ebc8c170e43b91121f13
hayoung0Lee <hayoung0.lee@gmail.com>
2021-02-18 22:47:52 +0900

    Communication between popup and background

--- src/background.js
+++ src/background.js
@@ -1,16 +1,15 @@
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: "#3aa757" }, function () {
-    console.log("The color is green.");
+  chrome.storage.local.set({ bookmarkStatus: false }, function () {
+    console.log("Value is set to " + false);
   });
-  chrome.storage.sync.set({ urls: [] }, function () {
-    console.log("The color is green.");
-  });
+
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([
       {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostContains: "youtube.com" },
+            // pageUrl: { hostContains: "google.com" },
+            // pageUrl: { hostContains: "youtube.com" },
           }),
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()],
@@ -19,18 +18,58 @@ chrome.runtime.onInstalled.addListener(function () {
   });
 });
 
-// FIXME: 이거 무조건 동작하게 개선하기(지금은 됬다 안됬다함)
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-  console.log("Changes");
-  for (var key in changes) {
-    var storageChange = changes[key];
-    console.log(
-      'Storage key "%s" in namespace "%s" changed. ' +
-        'Old value was "%s", new value is "%s".',
-      key,
-      namespace,
-      storageChange.oldValue,
-      storageChange.newValue
-    );
+// // FIXME: 이거 무조건 동작하게 개선하기(지금은 됬다 안됬다함)
+// chrome.storage.onChanged.addListener(function (changes, namespace) {
+//   console.log("Changes");
+//   for (var key in changes) {
+//     var storageChange = changes[key];
+//     console.log(
+//       'Storage key "%s" in namespace "%s" changed. ' +
+//         'Old value was "%s", new value is "%s".',
+//       key,
+//       namespace,
+//       storageChange.oldValue,
+//       storageChange.newValue
+//     );
+//   }
+// });
+
+// Background script is JavaScript code that's run as a separate
+// instance in the browser, and it's mostly used for listening to events
+// and to handle a browser wide state.
+
+let isBookMarkOpen = false;
+
+chrome.storage.local.get("bookmarkStatus", (res) => {
+  console.log("get updated", res["bookmarkStatus"]);
+  isBookMarkOpen = res["bookmarkStatus"];
+});
+
+// message format
+// {
+//   type: "TOGGLE_BOOKMARK",
+//   value: isBookMarkOpen
+// }
+const sendBookMarkStatus = (isBookMarkOpen) => {
+  chrome.runtime.sendMessage({
+    type: "BOOKMARK_STATUS",
+    value: isBookMarkOpen,
+  });
+};
+
+chrome.runtime.onMessage.addListener((request) => {
+  console.log("Message received in background.js!", request);
+
+  switch (request.type) {
+    case "TOGGLE_BOOKMARK":
+      isBookMarkOpen = request.value;
+      chrome.storage.sync.set({ bookmarkStatus: isBookMarkOpen });
+      sendBookMarkStatus(isBookMarkOpen);
+      break;
+    case "REQUEST_BOOKMARK_STATUS":
+      sendBookMarkStatus(isBookMarkOpen);
+      break;
+    default:
+      break;
   }
 });
--- src/popup.js
+++ src/popup.js
@@ -13,15 +13,40 @@
 //     chrome.tabs.executeScript(tabs[0].id, { file: "./slider.js" });
 //   });
 // };
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 const Popup = () => {
-  return <div>This is PopUp page</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    chrome.runtime.sendMessage({ type: "REQUEST_BOOKMARK_STATUS" });
+
+    chrome.runtime.onMessage.addListener((message) => {
+      switch (message.type) {
+        case "BOOKMARK_STATUS":
+          console.log("BOOKMARK_STATUS", message);
+          setIsOpen(message.value);
+          break;
+        default:
+          break;
+      }
+    });
+  }, []);
+  const handleBookMark = (e) => {
+    chrome.runtime.sendMessage({ type: "TOGGLE_BOOKMARK", value: !isOpen });
+  };
+
+  return (
+    <div>
+      This is PopUp page
+      <button onClick={handleBookMark}>
+        {isOpen ? `Close BookMark` : `Open BookMark`}
+      </button>
+    </div>
+  );
 };
 
 let root = document.getElementById("root");
-
-console.log("root", root);
 
 ReactDOM.render(<Popup />, root);
